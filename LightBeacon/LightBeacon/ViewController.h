cd3129ef1a907e9eb65df442380ee3a5df0d1208
Jon Fox <foxjon123@gmail.com>
2015-06-06 09:46:18 -0500

    Adds new project

---
+++ LightBeacon/LightBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightBeacon
+//
+//  Created by Jonathan Fox on 6/6/15.
+//  Copyright (c) 2015 Jon Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LightBeacon/LightBeacon/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LightBeacon
+//
+//  Created by Jonathan Fox on 6/6/15.
+//  Copyright (c) 2015 Jon Fox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
